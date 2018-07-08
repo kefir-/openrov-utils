ae190e04e2672a0044f93baf5d6d759b52fe990a
Ketil Froyn <ketil@froyn.name>
2018-07-08 14:29:56 +0200

    trident-metadata.py: - parse options - let user choose time format - let user choose whether to use gmtime() or localtime()

--- trident-metadata.py
+++ trident-metadata.py
@@ -5,6 +5,7 @@ import os
 import subprocess
 import json
 import time
+import getopt
 
 def usage():
     print("You're doing it wrong. Use the source, Luke.")
@@ -12,14 +13,46 @@ def usage():
 
 def main():
 
+    strftime_format = "%Y-%m-%dT%H%M%SUTC"
+    localtime = False
+
+    opts, args = getopt.getopt(sys.argv[1:], 'hstluf:')
+
+    for o, a in opts:
+        if o == '-f':
+            # specify strftime format 
+            strftime_format = a
+        elif o == '-h':
+            # display help, then exit
+            usage()
+        elif o == '-s':
+            # output standard Trident date string
+            strftime_format = "Trident-%b-%d-%H%M%S"
+        elif o == '-t':
+            # Output seconds since epoch
+            strftime_format = "%s"
+        elif o == '-l':
+            # use localtime (default is UTC / gmtime)
+            localtime = True
+        elif o == '-u':
+            # use UTC / gmtime (default is UTC / gmtime)
+            localtime = False
+
     dbdump_cmd=['''forestdb_dump''', '''--hex-key''', '''--hex-body''']
 
     try:
-        dbfile=sys.argv[1]
+        dbfile=args[0]
     except IndexError:
         usage()
 
-    dump = subprocess.check_output(dbdump_cmd + [dbfile]).decode('utf-8')
+    try:
+        command = dbdump_cmd + [dbfile]
+        dump = subprocess.check_output(command).decode('utf-8')
+    except Exception:
+        print("Error running command, bailing out:", command, file=sys.stderr)
+        sys.exit(1)
+
+    # find the first document body in the forestdb_dump output
     result = re.search('''\nDoc ID:.*?\n\s+Body:\s\(hex\)\s*\n(.*?)\n\n''', dump, flags=re.DOTALL|re.MULTILINE)
     body = None
     if not result:
@@ -31,17 +64,28 @@ def main():
     body = re.sub('''^\s+''', '', body, flags=re.MULTILINE)
     # strip trailing space and interpreted bytes on each line
     body = re.sub('''\s{3}.*$''', '', body, flags=re.MULTILINE)
-    # split to individual bytes
+    # split to individual bytes and decode bytes to binary string
     b = re.split('''\s+''', body)
     raw = ''.join([chr(int(x, 16)) for x in b])
+
+    # find the json string in the binary data
     result = re.search('''(\{[\w\s\"\{\}\[\]:\.,\-_]+\})''', raw)
     if not result:
         print("Couldn't recognize json in body data")
         sys.exit(1)
     j = result.group(1)
+
+    # parse json and fetch timestamp
     data = json.loads(j)
     sec = data['rx_ts']['sec']
-    ts_str = time.strftime("%Y-%m-%dT%H%M%SUTC", time.gmtime(sec))
+
+    timetuple = None
+    if localtime:
+        timetuple = time.localtime(sec)
+    else:
+        timetuple = time.gmtime(sec)
+
+    ts_str = time.strftime(strftime_format, timetuple)
     print(ts_str)
     # print(time.asctime(time.localtime(sec)))
     
