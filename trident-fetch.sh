1267aa280947708b744b602aad9340d5119dc0c4
Ketil Froyn <ketil@froyn.name>
2018-07-08 02:10:36 +0200

    Initial commit of trident-fetch.sh. Uses sweech to fetch files off the device, and tries to autodetect some details. Only tried it on my own setup.

---
+++ trident-fetch.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Sample wrapper to fetch 
+#
+
+SWEECH_URL=""
+TARGET_PATH="$HOME/openrov/"
+
+if [ ! -e "$TARGET_PATH" ]; then
+    mkdir -p "$TARGET_PATH"
+fi
+
+# Find where the URL has been set
+if [ -z $SWEECH_URL ] && [ ! -e $HOME/.config/sweech.json ]; then
+    echo "No SWEECH_URL given, and $HOME/.config/sweech.json doesn't exist."
+    echo "Please specify the URL somehow."
+    exit 1
+fi
+echo "DEBUG: SWEECH_URL=$SWEECH_URL (config file used if blank)"
+
+# Find the sweech.py or sweech command
+SWEECH_CMD="$HOME/git/sweech-cli/sweech.py"
+if [ ! -e "$SWEECH_CMD" ]; then
+    SWEECH_CMD="$(which sweech)"
+    if [ ! -e "$SWEECH_CMD" ]; then
+        echo "Can't find sweech.py, please fetch it from github or install it with 'pip install sweech-cli'"
+        exit 1
+    fi
+fi
+echo "DEBUG: Got sweech command: $SWEECH_CMD"
+
+# A bit of a hack to extract the base path of the internal storage on the device
+MOBILE_BASE="$("$SWEECH_CMD" info | grep -A3 "^Internal storage" | grep ^[[:space:]]*Path: | awk '{print $2}')"
+echo "DEBUG: MOBILE_BASE=$MOBILE_BASE"
+echo "DEBUG: TARGET_PATH=$TARGET_PATH"
+
+SOURCE="$MOBILE_BASE/Android/data/com.openrov.cockpit/files/data"
+echo "DEBUG: SOURCE=$SOURCE"
+
+exec "$SWEECH_CMD" pull --keep "$SOURCE" "$TARGET_PATH"
