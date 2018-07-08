28ec1abfca850cb3467bd008dd7bcfa89cfa9dcd
Ketil Froyn <ketil@froyn.name>
2018-07-08 16:41:51 +0200

    convert-new.sh: - more sensible output - Some better error checking

--- convert-new.sh
+++ convert-new.sh
@@ -2,12 +2,22 @@
 BASE=$HOME/openrov/data
 for fdb in $BASE/s_*.cblite2/db.forest.?; do
     id=$(echo "$fdb" | sed 's/^.*s_//; s/.cblite2.*$//')
-    echo "$id:$fdb";
+    # echo "$id:$fdb";
+    if [ ! -e $BASE/$id.h264 ]; then
+        echo "Got couchbase DB, but no matching .h264 video: $id"
+        continue
+    fi
     if [ ! -e $BASE/$id.mp4 ]; then
+        echo "Parsing metadata for $id"
         ts=$(trident-metadata.py -l -s "$fdb")
-        ffmpeg -r 30 -i $BASE/$id.h264 -c copy -r 30 $BASE/$ts.mp4
-        if [ $? -eq 0 ]; then
-            ln -sv $BASE/$ts.mp4 $BASE/$id.mp4
+        ts2=$(trident-metadata.py "$fdb")
+        # create the mp4 file if it hasn't already been done. Standardize on the
+        # format used internally by the OpenROV Cockpit app, for compatibility.
+        if [ ! -e $BASE/$ts.mp4 ]; then
+            echo "Converting $id.h264 to $ts.mp4 (alias $ts2.mp4)"
+            ffmpeg -v fatal -r 30 -i $BASE/$id.h264 -c copy -r 30 $BASE/$ts.mp4
         fi
+        ln -s $BASE/$ts.mp4 $BASE/$id.mp4
+        ln -s $BASE/$ts.mp4 $BASE/$ts2.mp4
     fi
 done
